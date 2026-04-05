73148a075d14d2cd4cce3863cd916a8570e4698d
afroze456 <afreenafroze1@gmail.com>
2026-04-05 14:38:48 -0700

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,9 @@
+print("enter in 4 subjects")
+math=int(input("math: "))
+english=int(input("english: "))
+hindi=int(input("hindi: "))
+science=int(input("science: "))
+sum= math+english+hindi+science
+perc=(sum/400)*100
+print(perc)
+print("the percentage of your scores are:",perc)
